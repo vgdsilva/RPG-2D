36cf40aa2c87a95296af6397d2c354c2ce41567c
vgdsilva <gasparellovinicius@gmail.com>
2021-01-04 14:58:09 -0300

    regeneração de HP e Mana

--- Assets/Scripts/Models/Entity.cs
+++ Assets/Scripts/Models/Entity.cs
@@ -8,28 +8,26 @@ public class Entity
 {
     [Header("Name")]
     public string name;
-
+    public int level;
 
     [Header("Health")]
     public int currentHealth;
     public int maxHealth;
 
-
     [Header("Mana")]
     public int currentMana;
     public int maxMana;
-
 
     [Header("Stamina")]
     public int currentStamina;
     public int maxStamina;
 
-
     [Header("Stats")]
     public int strength = 1;
     public int resistence = 1;
+    public int intelligence = 1;
+    public int willpower = 1;
     public int damage = 1;
     public int defense = 1;
     public float speed = 2f;
-
 }
